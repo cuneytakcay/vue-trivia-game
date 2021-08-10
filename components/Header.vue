ce9ff8efedc1713fa95a9ea97597c2b1e061feb3
cakcay <cuneyt.akcay@simpleviewinc.com>
2021-08-09 22:48:20 -0700

    Adding header component

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+	<header>
+		<h1>Trivia App</h1>
+		<li>{{ correctNumbers }}/{{ totalNumbers }}</li>
+	</header>
+</template>
+
+<script>
+export default {
+	props: ['correctNumbers', 'totalNumbers'],
+}
+</script>
